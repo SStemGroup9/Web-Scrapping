876a41ceb8f71c0daf671328ecf887f286187351
Titus0 <titus.lindsey@gmail.com>
2016-04-19 16:20:22 -0400

    Added files via upload
    Lindsey's build for graph with zoom in feature

--- build.php
+++ build.php
@@ -1,25 +1,32 @@
 <!DOCTYPE html>
 <meta charset="utf-8">
-<style> /* set the CSS */
+<style>
 
-body { font: 12px Arial;}
+svg {
+  font: 10px sans-serif;
+}
 
-path { 
-    stroke: steelblue;
-    stroke-width: 2;
-    fill: none;
+.area {
+  fill: steelblue;
+  clip-path: url(#clip);
 }
 
 .axis path,
 .axis line {
-    fill: none;
-    stroke: grey;
-    stroke-width: 1;
-    shape-rendering: crispEdges;
+  fill: none;
+  stroke: #000;
+  shape-rendering: crispEdges;
+}
+
+.brush .extent {
+  stroke: #fff;
+  fill-opacity: .125;
+  shape-rendering: crispEdges;
 }
 
 </style>
 <body>
+
 <?php
 $name=$_POST["name"];
 
@@ -39,47 +46,61 @@ file_put_contents('result.json',json_encode($data));
 #header( 'Location: http://student.cs.appstate.edu/yangh1');
 ?>
 
-<!-- load the d3.js library -->    
-<script src="http://d3js.org/d3.v3.min.js"></script>
 
-
-
+<script src="//d3js.org/d3.v3.min.js"></script>
 <script>
 
-// Set the dimensions of the canvas / graph
-var margin = {top: 30, right: 20, bottom: 30, left: 50},
-    width = 1000 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+var margin = {top: 10, right: 10, bottom: 100, left: 40},
+    margin2 = {top: 430, right: 10, bottom: 20, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom,
+    height2 = 500 - margin2.top - margin2.bottom;
 
-// Parse the date / time
 var parseDate = d3.time.format.utc("%Y-%m-%dT%H:%M:%S").parse;
 
-// Set the ranges
-var x = d3.time.scale().range([0, width]);
-var y = d3.scale.linear().range([height, 0]);
+var x = d3.time.scale().range([0, width]),
+    x2 = d3.time.scale().range([0, width]),
+    y = d3.scale.linear().range([height, 0]),
+    y2 = d3.scale.linear().range([height2, 0]);
 
-// Define the axes
-var xAxis = d3.svg.axis().scale(x)
-    .orient("bottom").ticks(15);
+var xAxis = d3.svg.axis().scale(x).orient("bottom"),
+    xAxis2 = d3.svg.axis().scale(x2).orient("bottom"),
+    yAxis = d3.svg.axis().scale(y).orient("left");
 
-var yAxis = d3.svg.axis().scale(y)
-    .orient("left").ticks(50);
+var brush = d3.svg.brush()
+    .x(x2)
+    .on("brush", brushed);
 
-// Define the line
-var valueline = d3.svg.line()
+var area = d3.svg.area()
+    .interpolate("monotone")
     .x(function(d) { return x(d.post_date); })
-    .y(function(d) { return y(d.num_likes); });
-    
-// Adds the svg canvas
-var svg = d3.select("body")
-    .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-        .attr("transform", 
-              "translate(" + margin.left + "," + margin.top + ")");
+    .y0(height)
+    .y1(function(d) { return y(d.num_likes); });
 
-// Get the data
+var area2 = d3.svg.area()
+    .interpolate("monotone")
+    .x(function(d) { return x2(d.post_date); })
+    .y0(height2)
+    .y1(function(d) { return y2(d.num_likes); });
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom);
+
+svg.append("defs").append("clipPath")
+    .attr("id", "clip")
+  .append("rect")
+    .attr("width", width)
+    .attr("height", height);
+
+var focus = svg.append("g")
+    .attr("class", "focus")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var context = svg.append("g")
+    .attr("class", "context")
+    .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
+
 d3.json("result.json", function(error, data) {
     data.forEach(function(d) {	
         d.post_date = parseDate(d.post_date.split("+")[0]);
@@ -88,28 +109,54 @@ d3.json("result.json", function(error, data) {
 	
     });
 
-    // Scale the range of the data
-    x.domain(d3.extent(data, function(d) { return d.post_date; }));
-    y.domain([0, d3.max(data, function(d) { return d.num_likes; })]);
 
-    // Add the valueline path.
-    svg.append("path")
-        .attr("class", "line")
-        .attr("d", valueline(data));
+    x.domain(d3.extent(data.map(function(d) { return d.post_date; })));
+    y.domain([0, d3.max(data.map(function(d) { return d.num_likes; }))]);
+    x2.domain(x.domain());
+    y2.domain(y.domain());
 
-    // Add the X Axis
-    svg.append("g")
+    focus.append("path")
+        .datum(data)
+        .attr("class", "area")
+        .attr("d", area);
+
+    focus.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + height + ")")
         .call(xAxis);
 
-    // Add the Y Axis
-    svg.append("g")
+    focus.append("g")
         .attr("class", "y axis")
         .call(yAxis);
 
+    context.append("path")
+        .datum(data)
+        .attr("class", "area")
+        .attr("d", area2);
+
+    context.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height2 + ")")
+        .call(xAxis2);
+
+    context.append("g")
+        .attr("class", "x brush")
+        .call(brush)
+      .selectAll("rect")
+        .attr("y", -6)
+        .attr("height", height2 + 7);
 });
+
+function brushed() {
+  x.domain(brush.empty() ? x2.domain() : brush.extent());
+  focus.select(".area").attr("d", area);
+  focus.select(".x.axis").call(xAxis);
+}
+
+function type(d) {
+  d.post_date = parseDate(d.post_date);
+  d.num_likes = +d.num_likes;
+  return d;
+}
+
 </script>
-
-
-</body>
